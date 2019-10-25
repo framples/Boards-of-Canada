8a9b420742cc8e0ed313ca04b32684cce8ec4f92
John Ochs <john.w.ochs0@gmail.com>
2019-10-24 19:16:16 -0500

    added homepage, now without functionality!

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,22 +3,19 @@ var db = require("../models");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    res.render("index", {
+      msg: "TM3K"
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  // app.get("/example/:id", function (req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Load manager page
   app.get("/manager", function (req, res) {
